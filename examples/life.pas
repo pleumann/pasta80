ea849f94f10f7cec170ca85c1d02571eb16e3190
pleumann <joerg.pleumann@gmail.com>
2025-03-16 18:47:03 +0100

    Improving Game of Life example a bit (still CP/M-only due to 80 columns)

--- examples/life.pas
+++ examples/life.pas
@@ -1,92 +1,110 @@
 program Life;
+
 const
-  Width    = 30;
+  Width    = 40;
   Height   = 20;
-  Percent  = 10;
+  Percent  = 20;
 
 var
-  Map: array[0..1] of array[0..19] of array[0..29] of Integer;
-  This, Next, Cycle, Live: Integer;
+  Map: array[0..1] of array[0..19] of array[0..39] of Byte;
+  This, Next, Cycle, Live, Color: Integer;
 
 procedure Paint;
 var
-  X,Y: Integer;
+  X, Y: Integer;
 begin
-  GotoXY(1,1);
+  GotoXY(1, 1);
   for Y := 0 to Height - 1 do
   begin
     for X := 0 to Width - 1 do
       if Map[This][Y][X] = 1 then
-        Write('#')
+      begin
+        TextBackground(Color);
+        Write('  ');
+        TextBackground(Black);
+      end
       else
-        Write(' ');
+        Write('  ');
     WriteLn;
   end;
   WriteLn;
+
+  Inc(Color);
+  if (Color = 8) then Color := 1;
 end;
 
 function Alive(X,Y: Integer): Boolean;
 var
-  L,R,U,D,Count: Integer;
+  L, R, U, D, Count: Integer;
 begin
-  if X = 0 then L := Width - 1 else L := X-1;
-  if Y = 0 then U := Height - 1 else U := Y-1;
+  if X = 0 then L := Width - 1 else L := X - 1;
+  if Y = 0 then U := Height - 1 else U := Y - 1;
 
-  if X = Width-1 then R := 0 else R := X+1;
-  if Y = Height-1 then D := 0 else D := Y+1;
+  if X = Width-1 then R := 0 else R := X + 1;
+  if Y = Height-1 then D := 0 else D := Y + 1;
 
   Count := Map[This][U][L] + Map[This][U][X] + Map[This][U][R]
          + Map[This][Y][L]                   + Map[This][Y][R]
          + Map[This][D][L] + Map[This][D][X] + Map[This][D][R];
 
-  Alive := (Map[This][Y][X]  = 1) and (Count=2) or (Count=3);
+  Alive := (Map[This][Y][X] = 1) and (Count=2) or (Count=3);
 end;
 
 procedure Think;
 var
-  X,Y,Temp: Integer;
+  X, Y, Temp: Integer;
 begin
-  Temp := This; This := Next; Next := Temp;
-
   Live := 0;
 
   for Y := 0 to Height - 1 do
     for X := 0 to Width  - 1 do
-      if Alive(X,Y) then
+      if Alive(X, Y) then
       begin
         Map[Next][Y][X] := 1;
         Live := Live + 1;
       end
       else
         Map[Next][Y][X] := 0;
+
+  Temp := This; This := Next; Next := Temp;
 end;
 
 procedure Setup;
 var
-  X,Y: Integer;
+  X, Y: Integer;
 begin
-  This := 0; Next := 1;
+  Live := 0; This := 0; Next := 1;
 
   for Y := 0 to Height - 1 do
     for X := 0 to Width - 1 do
       if Random(100) < Percent then
-        Map[Next][Y][X] := 1
+      begin
+        Map[This][Y][X] := 1;
+        Inc(Live);
+      end
       else
-        Map[Next][Y][X] := 0
+        Map[This][Y][X] := 0
 end;
-  
+
 begin
   ClrScr;
+  Randomize;
   Setup;
+
+  Color := 1;
 
   for Cycle := 1 to 800 do
   begin
-    TextColor(1 + Cycle mod 6);
+    CursorOn;
+    CheckBreak;
     CursorOff;
+
+    TextColor(1 + Cycle mod 6);
     Paint;
     TextColor(7);
-    Write('Cycle: ', Cycle,'  Alive: ', Live, '  ');
-    CursorOn;
+    Write(#27'p');
+    Write('  Cycle: ', Cycle:3,'  Alive: ', Live:3, '                          Press Ctrl-C to exit.  ');
+    Write(#27'q');
 
     Think;
   end;
