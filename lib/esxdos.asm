91ae0d861dc9c2fe07f0f4597411cd5ba1bedcd4
Jörg Pleumann <joerg.pleumann@gmail.com>
2025-03-19 14:51:43 +0100

    Prototype of esxDOS call finished, untested

--- lib/esxdos.asm
+++ lib/esxdos.asm
@@ -1,11 +1,45 @@
-esx_call:       ret
+; Calls an esxDOS function.
+;
+; In:   hl=esxDOS function number
+;       de=address of register record
+; Out:  hl=esxDOS error number, 0 means OK.
+;
+esx_call:       ld      a,(hl)
+                ld      (esx_call_num),a
+                ex      de,hl
 
+                push    hl
 
-;                ld      (esx_saved_sp),sp
-;                ld      sp,hl
-;                pop     bc
-;                pop     de
-;                pop     hl
-;                pop     af
-;                ld      sp,(esx_saved_sp)
-;                ei
+                di
+                ld      (esx_saved_sp),sp
+                ld      sp,hl
+                pop     af
+                pop     bc
+                pop     de
+                pop     ix                  ; HL when in dot command
+                ld      sp,(esx_saved_sp)
+                ei
+
+                rst     $08
+esx_call_num:   db      $00
+                pop     hl
+                jr      c,esx_call_err
+
+                di
+                ld      (esx_saved_sp),sp
+                ld      sp,hl
+                push    af
+                push    bc
+                push    de
+                ; push    ix                ; IX/HL not used in responses
+                ld      sp,(esx_saved_sp)
+                ei
+
+                ld      hl,0
+                ret
+
+esx_call_err:   ld      h,0
+                ld      l,a
+                ret
+
+esx_saved_sp:   ds      2
