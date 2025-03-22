4a25a4decf65ea76f3d2fa91828d11e748eaddf5
pleumann <joerg.pleumann@gmail.com>
2025-03-22 16:10:28 +0100

    Code cleanup and documentation for esxDOS support

--- lib/esxdos.asm
+++ lib/esxdos.asm
@@ -1,14 +1,16 @@
+; -------------------------------------------------------------------------
+; --- esxDOS assembly routines --------------------------------------------
+; -------------------------------------------------------------------------
+
 ; Calls an esxDOS function.
 ;
-; In:   hl=esxDOS function number
-;       de=address of register record
-; Out:  hl=esxDOS error number, 0 means OK.
+; In:           hl      esxDOS function number
+;               de      address of register record
+; Out:          hl      esxDOS error number, 0 is OK
 ;
-esx_call:       ld      a,(hl)
+esx_call:       ld      a,l
                 ld      (esx_call_num),a
                 ex      de,hl
-
-                push    hl
 
                 di
                 ld      (esx_saved_sp),sp
@@ -16,22 +18,23 @@ esx_call:       ld      a,(hl)
                 pop     af
                 pop     bc
                 pop     de
-                pop     ix                  ; HL when in dot command
+                pop     ix                  ; TODO HL when in dot command
+                ld      (esx_call_args),sp
                 ld      sp,(esx_saved_sp)
                 ei
 
                 rst     $08
 esx_call_num:   db      $00
-                pop     hl
+
                 jr      c,esx_call_err
 
                 di
-                ld      (esx_saved_sp),sp
-                ld      sp,hl
-                push    af
-                push    bc
+                ld      (esx_saved_sp),sp   ; This might be redundant
+                ld      sp,(esx_call_args)
+                push    ix                  ; Not actually used in results
                 push    de
-                ; push    ix                ; IX/HL not used in responses
+                push    bc
+                push    af
                 ld      sp,(esx_saved_sp)
                 ei
 
@@ -43,3 +46,4 @@ esx_call_err:   ld      h,0
                 ret
 
 esx_saved_sp:   ds      2
+esx_call_args:  ds      2
