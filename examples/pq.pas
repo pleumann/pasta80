8236bfcb1f93d7818ba7ab15824a5a190fbb1720
Jörg Pleumann <joerg.pleumann@gmail.com>
2025-04-03 14:25:50 +0200

    Fixed stupid bug in PQ example

--- examples/pq.pas
+++ examples/pq.pas
@@ -18,11 +18,9 @@ begin
 
   Write('p='); ReadLn(P);
   Write('q='); ReadLn(Q);
-
   WriteLn;
-
+  
   D := P * P / 4 - Q;
-
 
   if D < 0 then
     WriteLn('The equation is unsolvable!')
@@ -36,7 +34,9 @@ begin
   begin
     WriteLn('The equation has two solutions:');
     WriteLn;
-    WriteLn('x1=', -P / 2+Sqr(D):10:5);
-    WriteLn('x2=', -P / 2-Sqr(D):10:5);
+    WriteLn('x1=', -P / 2+Sqrt(D):10:5);
+    WriteLn('x2=', -P / 2-Sqrt(D):10:5);
   end;
+
+  WriteLn;
 end.
