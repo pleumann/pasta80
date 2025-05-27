43959c2ea255277d8f4d370e8f78471ce2c4357e
Jörg Pleumann <joerg.pleumann@gmail.com>
2025-05-27 13:45:37 +0200

    Adding overlay example

---
+++ tests/overlays.pas
@@ -0,0 +1,24 @@
+program Overlays;
+
+procedure A;
+begin
+end;
+
+overlay Procedure B1;
+begin
+end;
+
+overlay Procedure B2;
+begin
+end;
+
+procedure C;
+begin
+end;
+
+overlay Procedure D;
+begin
+end;
+
+begin
+end.
