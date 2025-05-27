01b190b67bd1a99a60193c4c3a2eeb77e6100eb6
Jörg Pleumann <joerg.pleumann@gmail.com>
2025-05-27 11:15:28 +0200

    Allowing overlay procedure or functions (currently dummy)

--- pasta.pas
+++ pasta.pas
@@ -6465,7 +6465,7 @@ var
   OldSyms, P: PSymbol;
   Name: String;
 begin
-  while Scanner.Token in [toConst, toType, toVar, toLabel, toProcedure, toFunction] do
+  while Scanner.Token in [toConst, toType, toVar, toLabel, toOverlay, toProcedure, toFunction] do
   begin
     if Scanner.Token = toConst then
     begin
@@ -6539,6 +6539,21 @@ begin
       end;
       Expect(toSemicolon);
       NextToken;
+    end
+
+    else if Scanner.Token = toOverlay then
+    begin
+      repeat
+        NextToken;
+
+        if not (Scanner.Token = toProcedure) or (Scanner.Token = toFunction) then
+          Error('Procedure or function expected');
+
+        if Level <> 0 then
+          Error('Overlays only allowed on top level');
+
+        ParseProcFunc(Sym);
+      until Scanner.Token <> toOverlay;
     end
 
     else if (Scanner.Token = toProcedure) or (Scanner.Token = toFunction) then
