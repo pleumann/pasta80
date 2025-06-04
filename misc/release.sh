a26df1651ecde486d1752796603c7e6e1fc86c80
Jörg Pleumann <joerg.pleumann@gmail.com>
2025-06-04 14:24:37 +0200

    Adding missing folder to release script

--- misc/release.sh
+++ misc/release.sh
@@ -2,7 +2,7 @@
 fpc pasta
 git clean -df .
 ZIP=pasta80-`./pasta --version`-`git rev-parse --short HEAD`-`uname -s`-`uname -m`.zip
-zip -r $ZIP LICENSE.txt README.md pasta docs examples rtl tests
+zip -r $ZIP LICENSE.txt README.md pasta docs examples misc rtl tests
 echo
 echo ------------------------------[ Release ready ]---------------------------------
 ls -lho $ZIP
