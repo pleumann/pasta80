ea446d53594480584873b50bacb3778f59fa0bf9
Jörg Pleumann <joerg.pleumann@gmail.com>
2026-03-11 12:55:59 +0100

    Reworked heap tests a bit

--- tests/heap.pas
+++ tests/heap.pas
@@ -4,93 +4,122 @@ var
   P: Pointer;
   Org: Integer;
 
-procedure DumpHeap;
+procedure DumpHeap(var Blocks, Total: Integer);
 var
   P: PBlock;
 begin
+  Blocks := 0;
+  Total := 0;
+
   P := HeapPtr;
   while P <> nil do
   begin
-    WriteLn(Ord(P), ':', Ord(P^.Next), P^.Size);
+    WriteLn('* ', Ord(P), ': Next=', Ord(P^.Next), ' Size=', P^.Size);
+    Inc(Blocks);
+    Inc(Total, P^.Size);
     P := P^.Next; 
   end;
+  WriteLn(Blocks, ' blocks, ', Total, ' bytes total');
 end;
 
-procedure SimpleTest;
+procedure TestSimple;
 var
-  P: Pointer;
+  P, Q: Pointer;
+  Blocks, Total, Max: Integer;
 begin
-  DumpHeap;
-  
-  GetMem(P, 256);
+  WriteLn('--- TestSimple ---');
+  WriteLn;
 
-  Writeln('P=', Ord(P));
-
-  DumpHeap;
-
-  Writeln('P=', Ord(P));
-
-  FreeMem(P, 256);
-
-  DumpHeap;
-
-  GetMem(P, 128);
-
-  DumpHeap;
-
-  FreeMem(P, 128);
-
-  DumpHeap;
-(*
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-  
-  FreeMem(P, 1000);
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-
-  GetMem(P, 1000);
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-  
-  FreeMem(P, 1000);
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-  
-  GetMem(P, 500);
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-  
-  FreeMem(P, 500);    
-  WriteLn('P: ', Ord(P), ' HeapPtr: ', Ord(HeapPtr), ' MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail);
-*)
-end;
-
-procedure HeapStress;
-var
-  P: array[0..128] of Pointer;
-  Size, I, Org: Integer;
-begin
   Org := MemAvail;
+  
+  DumpHeap(Blocks, Total);
+  Assert(Blocks = 1);
+  Assert(Total = MemAvail);
+  
+  WriteLn;
+  WriteLn('Allocating two blocks...');
+  GetMem(P, 256);
+  Writeln('P=', Ord(P));
+  GetMem(Q, 256);
+  Writeln('Q=', Ord(Q));
 
-  Size := 128;
-  while Size >= 4 do
-  begin
-    Writeln('Block size=', Size);
-    for I := 0 to 127 do
-      GetMem(P[I], Size);
+  DumpHeap(Blocks, Total);
+  Assert(Blocks = 1);
+  Assert(Total = MemAvail);
 
-    Assert(MemAvail < Org);
-
-    for I := 0 to 127 do
-      FreeMem(P[I], Size);
-    Size := Size - 4;
-
-    Assert(MemAvail = Org);
-  end;
+  Max := Total;
 
   WriteLn;
+  WriteLn('Freeing first block...');
+  FreeMem(P, 256);
+  DumpHeap(Blocks, Total);
+  Assert(Blocks = 2);
+  Assert(Total = MemAvail);
+
+  WriteLn;
+  WriteLn('Freeing second block...');
+  FreeMem(Q, 256);
+  DumpHeap(Blocks, Total);
+  Assert(Blocks = 3);
+  Assert(Total = MemAvail);
+
+  Assert(MemAvail = Org);
+  Assert(MaxAvail = Max);
+end;
+
+procedure TestStress;
+var
+  P: array[0..255] of Pointer;
+  Size, I, J, K, Org: Integer;
+begin
+  WriteLn('--- TestStress ---');
+  WriteLn;
+
+  Org := MemAvail;
+
+  Size := 1024;
+  while Size >= 4 do
+  begin
+    WriteLn('Block size =', Size:4);
+    Write('Block count=   0');
+    I := 0;
+    while (I < 256) and (MaxAvail >= Size)do
+    begin
+      GetMem(P[I], Size);
+      Inc(I);
+      Write(#8#8#8#8, I:4);
+    end;
+
+    WriteLn;
+    Assert(MemAvail < Org);
+
+    for J := 0 to 9 do
+    begin
+      K := Random(I);
+      if P[K] <> nil then
+      begin
+        FreeMem(P[K], Size);
+        P[K] := nil;
+      end;
+    end;
+
+    for J := 0 to I - 1 do
+    begin
+      if P[J] <> nil then FreeMem(P[J], Size);
+    end;
+
+    Size := Size div 2;
+
+    Assert(MemAvail = Org);
+
+    WriteLn;
+  end;
 end;
 
 type
   PNode = ^TNode;
   TNode = record
-    Data: string;
+    Data: string[30];
     Next: PNode;
   end;
 
@@ -119,21 +148,11 @@ begin
   Dispose(P);
 end;
 
+procedure TestNewDispose;
 var
   TheFullMonty: PNode;
-
 begin
-  WriteLn;
-  WriteLn('*** PASTA/80 Test Suite ***');
-  WriteLn;
-
-  WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
-  WriteLn;
-
-  HeapStress;
-
-  WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
-  WriteLn;
+  WriteLn('--- TestNewDispose ---');
 
   Org := MemAvail;
 
@@ -145,6 +164,7 @@ begin
     'Aren''t 3 lives enough to last'),
     'Why did Monty die so fast?');
 
+  WriteLn;  
   WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
   WriteLn;  
 
@@ -157,6 +177,28 @@ begin
   WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
 
   Assert(MemAvail = Org);
+end;
+
+begin
+  WriteLn;
+  WriteLn('*** PASTA/80 Test Suite ***');
+  WriteLn;
+
+  WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
+  WriteLn;
+
+  TestSimple;
+
+  WriteLn;
+  WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
+  WriteLn;
+
+  TestStress;
+
+  WriteLn('MemAvail: ', MemAvail, ' MaxAvail: ', MaxAvail, ' HeapPtr: ', Ord(HeapPtr));
+  WriteLn;
+
+  TestNewDispose;
 
   WriteLn;
   WriteLn('************************');
