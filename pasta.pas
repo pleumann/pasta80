9eb2ad9812999c2b39fca67eae792284ae33751d
Jörg Pleumann <joerg.pleumann@gmail.com>
2026-03-12 10:42:18 +0100

    Cycle correctly through binary types no matter what

--- pasta.pas
+++ pasta.pas
@@ -7868,6 +7868,8 @@ begin
 
   if Binary = btZXN then
     WriteLn(BinaryStr[Binary] + ', Z80N':40)
+  else if Binary = btAgon then
+    WriteLn(BinaryStr[Binary] + ', eZ80':40)
   else
     WriteLn(BinaryStr[Binary] + ', Z80':40);
   WriteLn;
@@ -7901,8 +7903,8 @@ begin
     C := GetKey;
     case C of
       'm': begin
-             if Binary = btZXN then
-               Binary := btCPM
+             if Binary = High(TBinaryType) then
+               Binary := Low(TBinaryType)
              else
                Binary := Succ(Binary);
 
