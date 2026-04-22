79d67dca204b0138921b71c79c8e931ad31e7d0a
Jörg Pleumann <joerg.pleumann@gmail.com>
2026-04-22 20:56:53 +0200

    Making sure we don't include .DS_Store on MacOS

--- misc/release.sh
+++ misc/release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 fpc pasta
 git clean -df .
+find . -name .DS_Store -delete
 ZIP=pasta80-`./pasta --version`-`git rev-parse --short HEAD`-`uname -s`-`uname -m`.zip
 zip -r $ZIP LICENSE.txt README.md pasta docs examples misc rtl tests
 echo
