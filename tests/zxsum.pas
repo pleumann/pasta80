619c6a0d63bb029dcdfa3e6917a2303d4e62cbc5
Jörg Pleumann <joerg.pleumann@gmail.com>
2026-06-17 10:30:51 +0200

    Updated wording a bit

--- tests/zxsum.pas
+++ tests/zxsum.pas
@@ -1,24 +1,27 @@
 (**
- * Example for exchanging data between classic ZX Spectrum BASIC and Pascal. We
- * use the printer buffer for passing data from BASIC to Pascal and the BC
+ * Example for exchanging data between a classic ZX Spectrum BASIC and Pascal.
+ * We use the printer buffer for passing data from BASIC to Pascal and the BC
  * register pair for passing a 16 bit value back to BASIC. When the program
- * terminates, BC receives its value from the ExitCode variable (which can also
- * be set by the Halt procedure).
+ * terminates, BC is assigned the value of the ExitCode global variable (which
+ * can also be set by the Halt procedure).
  *
- * Suitable modified BASIC loader could look like this:
+ * A suitable modified BASIC loader could look like this:
  *
  * 10 CLEAR 32767
  * 20 LOAD "bin" CODE 32768
- * 30 FOR n=0 to 255: POKE 23296+n, n: NEXT n
+ * 30 FOR n=0 TO 255: POKE 23296+n,n: NEXT n
  * 40 PRINT USR 32768
  *
  * This should print 32640, which is the sum of all numbers from 0 to 255.
+ *
+ * The printer buffer can also be used for passing data back to BASIC, in case
+ * a single 16 bit Integer value is not sufficient.
  *)
 program ZXSum;
 
 {$ifndef SYS_ZX48}
   {$ifndef SYS_ZX128}
-    {$error This program only work on classic Spectrums.}
+    {$error This program only works on classic Spectrums.}
   {$endif}
 {$endif}
 
